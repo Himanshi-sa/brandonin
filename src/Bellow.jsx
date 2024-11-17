dfa7a006d2928623fac60d2411d7b24be0f99d5c
Himanshi-sa <himanshisahuhs@gmail.com>
2024-11-17 09:20:01 +0000

    final

---
+++ src/Bellow.jsx
@@ -0,0 +1,19 @@
+import Accordion from "./Accordion";
+
+function Bellow({ data }) {
+  return (
+    <div>
+      {data.map((obj) => {
+        return (
+          <Accordion
+            key={obj.id}
+            title={obj.title}
+            description={obj.description}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+export default Bellow;
